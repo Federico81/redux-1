dae8c2b45787740ae390394f44694bc44219f48c
Federico81 <federico.deambrosis@gmail.com>
2024-01-15 12:03:58 +0100

    Delete src directory

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-//actions/index.js
-export const increment = () => ({ type: 'INCREMENT' });
-export const decrement = () => ({ type: 'DECREMENT' });
-
-export const setUser = (user) => ({ type: 'SET_USER', payload: user });
-export const clearUser = () => ({ type: 'CLEAR_USER' });
